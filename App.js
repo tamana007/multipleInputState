dfc6caefac2ec09f62476bb25eccd05a1ba52334
tamana007 <tamana.efatwira1@gmail.com>
2023-10-06 11:57:57 -0700

    Mutating btw rerenders

--- App.js
+++ App.js
@@ -4,75 +4,90 @@ import { useEffect, useRef, useState } from 'react';
 
 function App() {
 
-  const refcontainer=useRef('');
+  const refContainer=useRef('');
+  const[value,setValue]=useState('0');
 
-  function inputfunc(e){
-// console.log(e.target.value);
-refcontainer.current.value=' ';
-// refcontainer.current.focus();
+  function handlBTN(){
+    refContainer.current++;
+    setValue(refContainer.current);
   }
-
-  // const[people,setPeople]=useState([{Name:"ali",Password:"1232",email:"ali@gmail.com"}]);
-  const [shipping,setShipping]=useState(false);
-  const [program,setprogram]=useState("programs")
-  // const list=[{Name:"ali",Password:"1232",email:"ali@gmail.com"}]
-  // setPeople(list);
-  
-  //:::::::::::::::::::::::::::::::::::::::::::::::Reverse Algorithm:::::::::::::::::::::::::::
-  // function resverseStr(){
-  //   const str="Ali";
-  //   const reserve=str.split([]).reverse().join();
-  //   return reserve;
-
-
-  // }
-  const list=["Java","React","Angular"];
-
-  function dropfunc(e){
-    e.preventDefault();
-    setprogram(e.target.value);
-  }
-  
-  
-  useEffect(()=>{
-    console.log(program);
-
-  },[program])
-
-  function inputFunc(e){
-    // e.preventDefault();
-    setShipping(e.target.checked);
-// setPeople({...people},{e.target.Name:e.target.value})
-// setPeople({...people,[e.target.Name]:e.target.value})
-// setPeople(e.target.value);
-console.log(shipping);
-
-  }
-  return (
-    // <form onSubmit={inputFunc}>
+  return(
     <>
-    <input type='text' ref={refcontainer}/>
-      <label>Free Shipping</label>
-      <input name='name'  type='checkbox' checked={shipping} onChange={inputFunc}/>
-      <label htmlFor='Shipping' name='shipement'>shapement</label>
-      {/* <input name='email' value={people.email}  /> */}
-      <button type='submit' onClick={inputfunc}>Submit</button>
-
-
-      <select name='framework' onChange={dropfunc}>
-            <option value=" ">select a program</option>
-            {list.map((item)=>{
-          return(
-            <option value={item}>{item}</option>
-            
-          )
-        })}
-      </select>
-
-
-    {/* // </form> */}
+    <p>testing mutating or persisting btw re-renders:{refContainer.current}</p>
+    <h4>{value}</h4>
+    <button onClick={handlBTN}>Click here</button>
     </>
-  );
+  )
+
+//   const refcontainer=useRef('');
+
+//   function inputfunc(e){
+// // console.log(e.target.value);
+// refcontainer.current.value=' ';
+// refcontainer.current.focus();
+//   }
+
+//   // const[people,setPeople]=useState([{Name:"ali",Password:"1232",email:"ali@gmail.com"}]);
+//   const [shipping,setShipping]=useState(false);
+//   const [program,setprogram]=useState("programs")
+//   // const list=[{Name:"ali",Password:"1232",email:"ali@gmail.com"}]
+//   // setPeople(list);
+  
+//   //:::::::::::::::::::::::::::::::::::::::::::::::Reverse Algorithm:::::::::::::::::::::::::::
+//   // function resverseStr(){
+//   //   const str="Ali";
+//   //   const reserve=str.split([]).reverse().join();
+//   //   return reserve;
+
+
+//   // }
+//   const list=["Java","React","Angular"];
+
+//   function dropfunc(e){
+//     e.preventDefault();
+//     setprogram(e.target.value);
+//   }
+  
+  
+//   useEffect(()=>{
+//     console.log(program);
+
+//   },[program])
+
+//   function inputFunc(e){
+//     // e.preventDefault();
+//     setShipping(e.target.checked);
+// // setPeople({...people},{e.target.Name:e.target.value})
+// // setPeople({...people,[e.target.Name]:e.target.value})
+// // setPeople(e.target.value);
+// console.log(shipping);
+
+//   }
+//   return (
+//     // <form onSubmit={inputFunc}>
+//     <>
+//     <input type='text' ref={refcontainer}/>
+//       <label>Free Shipping</label>
+//       <input name='name'  type='checkbox' checked={shipping} onChange={inputFunc}/>
+//       <label htmlFor='Shipping' name='shipement'>shapement</label>
+//       {/* <input name='email' value={people.email}  /> */}
+//       <button type='submit' onClick={inputfunc}>Submit</button>
+
+
+    //   <select name='framework' onChange={dropfunc}>
+    //         <option value=" ">select a program</option>
+    //         {list.map((item)=>{
+    //       return(
+    //         <option value={item}>{item}</option>
+            
+    //       )
+    //     })}
+    //   </select>
+
+
+    // {/* // </form> */}
+    // </>
+  // );
 }
 
 export default App;
